2432f9fedfe265646e4b68aaaddd45782f77ec8c
AvatarMalekar <avatarmalekar4@gmail.com>
2020-01-24 17:24:31 +0530

    ADDED:single coin flip

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,2 +1,11 @@
-#!/bin/bash -x
+#!/bin/bash
 echo "-------------------------------------------WELLOME FLIP COIN SIMULATION PROBLEM--------------------------------------------------"
+HEAD=0;
+TAIL=1;
+flipCoin=$((RANDOM%2))
+if [ $flipCoin -eq $HEAD ]
+then
+	echo "HEAD"
+else
+	echo "TAIL"
+fi
