ed00d9534222a12dad6561f956b0b95756a962e6
AvatarMalekar <avatarmalekar4@gmail.com>
2020-01-24 18:20:21 +0530

    ADDED:created dictionary stored coin flip combination and calculated percentage

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -2,10 +2,17 @@
 echo "-------------------------------------------WELLOME FLIP COIN SIMULATION PROBLEM--------------------------------------------------"
 HEAD=0;
 TAIL=1;
+declare -A SingleCoinFlipDictionary
+for (( i=1; i<=12; i++ ))
+do
 flipCoin=$((RANDOM%2))
-if [ $flipCoin -eq $HEAD ]
-then
-	echo "HEAD"
-else
-	echo "TAIL"
-fi
+	if [ $flipCoin -eq $HEAD ]
+	then
+		SingleCoinFlipDictionary[HEAD]=$((${SingleCoinFlipDictionary[HEAD]}+1*100/12))
+	else
+		 SingleCoinFlipDictionary[TAIL]=$((${SingleCoinFlipDictionary[TAIL]}+1*100/12))
+	fi
+done
+echo ${!SingleCoinFlipDictionary[@]}
+echo "Percentage respectively:"
+echo ${SingleCoinFlipDictionary[@]}
