9925cc263455e71e31b6787297b3bd74ba36a346
AvatarMalekar <avatarmalekar4@gmail.com>
2020-01-25 12:10:02 +0530

    MODIFIED:modified program completely ADDED:triplet simulation along with percentage

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -2,42 +2,46 @@
 echo "-------------------------------------------WELLOME FLIP COIN SIMULATION PROBLEM--------------------------------------------------"
 HEAD=0;
 TAIL=1;
-declare -A SingleCoinFlipDictionary
-declare -A DoubleCoinFlipDictionary
-for (( i=1; i<=12; i++ ))
+declare -A CoinFlipDictionary
+declare -A coinFlipDictonaryPercentage
+function flipCoin(){
+for (( i=0; i<$2; i++  ))
 do
-flipCoin=$((RANDOM%2))
-	if [ $flipCoin -eq $HEAD ]
-	then
-		SingleCoinFlipDictionary[HEAD]=$((${SingleCoinFlipDictionary[HEAD]}+1*100/12))
-	else
-		 SingleCoinFlipDictionary[TAIL]=$((${SingleCoinFlipDictionary[TAIL]}+1*100/12))
-	fi
+	str=""
+	for (( j=0; j<$1; j++ ))
+	do
+		flip=$((RANDOM%2))
+		if [ $flip -eq $HEAD ]
+		then
+			str="${str}H"
+		else
+			str="${str}T"
+		fi
+	done
+	CoinFlipDictionary[$str]=$((${CoinFlipDictionary[$str]}+1))
+	coinFlipDictonaryPercentage[$str]=$((${coinFlipDictonaryPercentage[$str]}+1*100/$2))
 done
-echo "Single coin fliped.."
-echo ${!SingleCoinFlipDictionary[@]}
-echo "Percentage respectively:"
-echo ${SingleCoinFlipDictionary[@]}
+}
 
-echo "------------------------------------------------------------------------------------------------------------------"
-for (( j=1; j<=12; j++ ))
-do
-firstFlip=$((RANDOM%2))
-secondFlip=$((RANDOM%2))
-	if [ $firstFlip -eq $HEAD -a $secondFlip -eq $HEAD ]
-	then
-		DoubleCoinFlipDictionary[HEADHEAD]=$((${DoubleCoinFlipDictionary[HEADHEAD]}+1*100/12))
-	elif [ $firstFlip -eq $HEAD -a $secondFlip -eq $TAIL ]
-	then
-		DoubleCoinFlipDictionary[HEADTAIL]=$((${DoubleCoinFlipDictionary[HEADTAIL]}+1*100/12))
-	elif [ $firstFlip -eq $TAIL -a $secondFlip -eq $HEAD ]
-	then
-		DoubleCoinFlipDictionary[TAILHEAD]=$((${DoubleCoinFlipDictionary[TAILHEAD]}+1*100/12))
-	else
-		DoubleCoinFlipDictionary[TAILTAIL]=$((${DoubleCoinFlipDictionary[TAILTAIL]}+1*100/12))
-	fi
-done
-echo "Two coins fliped..."
-echo ${!DoubleCoinFlipDictionary[@]}
-echo "Perentage Respectively"
-echo ${DoubleCoinFlipDictionary[@]}
+echo "1.One Coin"
+echo "2.Two Coins"
+echo "3.Three Coins"
+
+echo "Enter the how many coins you want to flip:"
+read -p "Enter Your choice:" coinCount
+if [ $coinCount -lt 4 ]
+then
+	read -p "Enter the how many times:" flipCount
+else
+	echo "Invalid choice"
+	coinCount=0
+fi
+echo "------------------------------------------------------------------------------------------------------------------------------"
+flipCoin $coinCount $flipCount
+echo "Coin Simulaton Respectively:"
+echo ${!CoinFlipDictionary[@]}
+echo ${CoinFlipDictionary[@]}
+echo "------------------------------------------------------------------------------------------------------------------------------"
+echo "Coin Percentage Respectively:"
+echo ${!coinFlipDictonaryPercentage[@]}
+echo ${coinFlipDictonaryPercentage[@]}
